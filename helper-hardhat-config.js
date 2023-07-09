56217f601d731abe7eb6668a6ef30f94d93437f6
Daudit <dimo.dev@outlook.com>
2023-07-09 22:35:08 +0300

    feat(constants): Add mintFee to helper-hardhat-config

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -6,12 +6,14 @@ const networkConfig = {
         interval: "30", //30 sec
         callbackGasLimit: "500000", // 500,000 gas
         vrfCoordinatorV2: "0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625",
+        mintFee: "10000000000000000", //0.01 ether
     },
     31337: {
         name: "hardhat",
         gasLane: "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c", // 30 gwei
         interval: "30",
         callbackGasLimit: "500000",
+        mintFee: "10000000000000000", //0.01 ether
     },
 }
 const DECIMALS = "18"
