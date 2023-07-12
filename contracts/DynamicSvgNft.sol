c728a01758057acf987ff9844e6563e351ca0e5a
Daudit <dimo.dev@outlook.com>
2023-07-12 21:39:47 +0300

    feat(dynamicSvgNft): Add encoding of token

--- contracts/DynamicSvgNft.sol
+++ contracts/DynamicSvgNft.sol
@@ -3,6 +3,7 @@
 pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "base64-sol/base64.sol";
 
 contract DynamicSvgNft is ERC721 {
     uint256 private s_tokenCounter;
@@ -15,12 +16,39 @@ contract DynamicSvgNft is ERC721 {
         s_tokenCounter = 0;
     }
 
-    function svgToImageURI() public pure returns (string memory){
-        
+    function svgToImageURI(string memory svg) public pure returns (string memory) {
+        string memory svgBase64Encoded = Base64.encode(bytes(string(abi.encodePacked(svg))));
+        return string(abi.encodePacked(base64EncodedSvgPrefix, svgBase64Encoded));
     }
 
     function mintNft() public {
         _safeMint(msg.sender, s_tokenCounter);
         s_tokenCounter += 1;
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "data:application/json;base64,";
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        require(_exists(tokenId), "URI Query for nonexistent token");
+        string memory imageURI = "hi!";
+        string(
+            abi.encodePacked(
+                _baseURI(),
+                Base64.encode(
+                    bytes(
+                        abi.encodePacked(
+                            '{"name":"',
+                            name(), // You can add whatever name here
+                            '", "description":"An NFT that changes based on the Chainlink Feed", ',
+                            '"attributes": [{"trait_type": "coolness", "value": 100}], "image":"',
+                            imageURI,
+                            '"}'
+                        )
+                    )
+                )
+            )
+        );
     }
 }
