9711122ff3dc9964a47f823937aa1f2e97c90573
Daudit <dimo.dev@outlook.com>
2023-07-12 22:28:45 +0300

    fix(dynamicSvgNft): Fix small issues

--- deploy/00-deploy-mocks.js
+++ deploy/00-deploy-mocks.js
@@ -3,8 +3,6 @@ const { DECIMALS, INITIAL_PRICE } = require("../helper-hardhat-config")
 
 const BASE_FEE = "250000000000000000" // 0.25 is this the premium in LINK?
 const GAS_PRICE_LINK = 1e9 // link per gas, is this the gas lane? // 0.000000001 LINK per gas
-const DECIMALS = "18"
-const INITIAL_PRICE = ethers.utils.parseUnits("2000", "ether")
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments
@@ -19,7 +17,7 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
             args: [BASE_FEE, GAS_PRICE_LINK],
         })
 
-        await deploy("MockV4Aggregator", {
+        await deploy("MockV3Aggregator", {
             from: deployer,
             log: true,
             args: [DECIMALS, INITIAL_PRICE],
